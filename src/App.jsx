6d57732fbd76b74eed9ac6ce4a46b796ae26c552
Andy Truong <andytruongcs@gmail.com>
2025-04-10 23:56:04 -0400

    Added description

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,14 @@ import './App.css';
 import { Loader2 } from 'lucide-react';
 const apiKey = import.meta.env.VITE_MOVIE_DB_API_KEY;
 import { Tv, Clapperboard } from 'lucide-react';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
 
 function App() {
   const [movieData, setMovieData] = useState([]);
@@ -17,6 +25,7 @@ function App() {
       let resp = await axios.get(
         `https://api.themoviedb.org/3/trending/${type}/day?api_key=${apiKey}&media_type=movie`
       );
+      console.log(resp.data.results);
       setMovieData(resp.data.results);
     } catch (e) {
       console.log(22, e);
@@ -28,6 +37,7 @@ function App() {
       <div className='background_container'>
         <div className='button_container'>
           <button
+            className='text-2xl rounded-lg border border-[#032541] bg-[#084b81] text-white min-w-[300px] mt-5'
             onClick={() => {
               getTrendingMovieData('movie');
             }}
@@ -36,10 +46,12 @@ function App() {
               <div className='mx-[1rem]'>
                 <Clapperboard />
               </div>
-              <div>Trending Movies</div>
+              <div className='p-3 text-3xl'>Trending Movies</div>
             </div>
           </button>
+
           <button
+            className='text-2xl rounded-lg border border-[#032541] bg-[#084b81] text-white min-w-[300px] mt-5'
             onClick={() => {
               getTrendingMovieData('tv');
             }}
@@ -48,28 +60,43 @@ function App() {
               <div className='mx-[1rem]'>
                 <Tv />
               </div>
-              <div>Trending TV</div>
+              <div className='p-3 text-3xl'>Trending TV</div>
             </div>
           </button>
         </div>
-        <div class='flex flex-wrap bg-[#032541] justify-center'>
+        <div className='flex flex-wrap bg-[#032541] justify-center'>
           {movieData.length !== 0 ? (
             <>
               {movieData.map((item) => (
-                <div className='movie_item'>
-                  <div className='text-center'>
-                    <img
-                      className='w-[300px]'
-                      alt={'Movie or tv show'}
-                      src={`https://image.tmdb.org/t/p/w500/${item.poster_path}`}
-                    />
-                    <div className='movie_name'>
-                      {item.original_title
-                        ? item.original_title
-                        : item.original_name}
+                <Dialog>
+                  <DialogTrigger>
+                    <div className='movie_item'>
+                      <div className='text-center'>
+                        <img
+                          className='w-[300px]'
+                          alt={'Movie or tv show'}
+                          src={`https://image.tmdb.org/t/p/w500/${item.poster_path}`}
+                        />
+                        <div className='movie_name'>
+                          {item.original_title
+                            ? item.original_title
+                            : item.original_name}
+                        </div>
+                      </div>
                     </div>
-                  </div>
-                </div>
+                  </DialogTrigger>
+
+                  <DialogContent className='text-bold bg-[#032541]'>
+                    <DialogHeader>
+                      <DialogTitle className='text-white text-2xl'>
+                        Overview:
+                      </DialogTitle>
+                      <DialogDescription className='text-white text-xl'>
+                        {item.overview}
+                      </DialogDescription>
+                    </DialogHeader>
+                  </DialogContent>
+                </Dialog>
               ))}
             </>
           ) : (
